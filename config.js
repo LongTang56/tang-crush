0e122ba05bdec922f181898a69573d84eeb850a3
LongTang56 <86094355+LongTang56@users.noreply.github.com>
2021-06-18 12:40:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Anh luôn thấy nhớ em mỗi ngày <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Yêu em. Muốn hôn e ghê.',
     btnAccept: 'Okiiiii lun <3',
